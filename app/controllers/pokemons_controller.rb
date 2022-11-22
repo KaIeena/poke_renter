d8870bb0028c63b6cc5721a1424d0d3a8028348e
louis nicoletti <louis.nicoletti@gmail.com>
2022-11-22 11:18:30 +0100

    view controller pkm

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,40 @@
+class PokemonsController < ApplicationController
+  def index
+    @pokemon = Pokemon.all
+  end
+
+  def show
+
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+
+    if @pokemon.save
+      redirect_to @pokemon, notice: "Pokemon was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @pokemon = Pokemon.new
+  end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  # def destroy
+  # end
+
+  private
+
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :type, :hp, :atk, :def, :spd, :cap1, :cap2, :cap3, :cap4, :user_id)
+  end
+
+end
